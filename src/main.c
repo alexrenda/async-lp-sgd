35eec5e0750b7bf4f9520b310b7221aec4754309
Matthew Gharrity <mjg355@cornell.edu>
2017-11-07 18:22:13 -0500

    Add Makefiles

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#ifdef OSX_ACCELERATE
+#  include <Accelerate/Accelerate.h>
+#elif defined(__GNUC__) || defined(__GNUG__)
+#  include <cblas.h>
+#endif
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, world!\n");
+}
