56014bbf74e7dbfc5eeb4c3dbd49bcb27539b93c
Alex Renda <alex@renda.org>
2017-11-11 16:15:01 -0500

    fix mnist restrict

--- src/mnist.h
+++ src/mnist.h
@@ -4,8 +4,8 @@
 typedef struct dataset_t {
   const int N;                  // Number
   const int dim;                // Dimensions of an image
-  char* const restrict labels; // Labels: byte[n]
-  float* const restrict image;  // Images: byte[n][dim*dim]
+  char* const __restrict__ labels; // Labels: byte[n]
+  float* const __restrict__ image;  // Images: byte[n][dim*dim]
 } dataset_t;
 
 dataset_t* get_train_dataset();
