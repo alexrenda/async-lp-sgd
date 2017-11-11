6b8d06e5b40a1de90f85f8836fe90535c954e193
Alex Renda <alex@renda.org>
2017-11-11 15:05:06 -0500

    base SGD implementation

--- src/main.c
+++ src/main.c
@@ -5,7 +5,64 @@
 #endif
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
+  srand(1234);
+
+
+
   printf("Hello, world!\n");
+}
+
+// https://stackoverflow.com/a/6852396
+// Assumes 0 <= max <= RAND_MAX
+// Returns in the closed interval [0, max]
+long random_at_most(long max) {
+  unsigned long
+    // max <= RAND_MAX < ULONG_MAX, so this is okay.
+    num_bins = (unsigned long) max + 1,
+    num_rand = (unsigned long) RAND_MAX + 1,
+    bin_size = num_rand / num_bins,
+    defect   = num_rand % num_bins;
+
+  long x;
+  do {
+    x = random();
+  }
+  // This is carefully written not to overflow
+  while (num_rand - defect <= (unsigned long)x);
+
+  // Truncated division is intentional
+  return x/bin_size;
+}
+
+void sgd(float* __restrict__ w,
+         const float* __restrict__ xs,
+         const char* __restrict__ ys,
+         const size_t n,
+         const size_t d,
+         const unsigned int niter,
+         const float alpha,
+         const float lambda
+         ) {
+
+  memset(w, 0, sizeof(float) * d);
+
+  for (int i = 0; i < niter; i++) {
+    const int idx = random_at_most(n);
+    const float *x = &xs[idx * d];
+    const char y = ys[idx];
+
+    float wTx = 0;
+    for (int j = 0; j < d; j++) {
+      wTx += x[j] * w[j];
+    }
+
+    const float scale = -y / (1 + expf(y * wTx));
+
+    for (int j = 0; j < d; j++) {
+      w[j] -= alpha * (scale * x[j] + 2*lambda*w[j]);
+    }
+  }
 }
