db3126818b152766d28bd214658926fc82bc607b
Alex Renda <alex@renda.org>
2017-11-21 23:21:40 -0800

    rough multithread timing

--- src/timing.hpp
+++ src/timing.hpp
@@ -3,33 +3,48 @@
 
 #include <omp.h>
 #include <cassert>
+#include <atomic>
 
 class timing_t {
   double _total_time;
   int _steps;
   double _start_time;
+  std::atomic<unsigned int> _entrants;
 
 public:
-  timing_t(): _total_time(0), _steps(0), _start_time(-1) {}
+  timing_t(): _total_time(0), _steps(0), _start_time(-1), _entrants(0) {}
 
   void start_timing_round() {
-    _start_time = omp_get_wtime();
+    if (_entrants == 0) {
+      _start_time = omp_get_wtime();
+    }
+    _entrants++;
   }
 
   void end_timing_round(int _stepsTaken) {
-    assert(_start_time >= 0);
+    assert(_entrants > 0);
+    unsigned int m_entrants = --_entrants;
 
-    _total_time += omp_get_wtime() - _start_time;
-    _steps += _stepsTaken;
-    _start_time = -1;
+    if (m_entrants == 0) {
+      _total_time += omp_get_wtime() - _start_time;
+      _steps += _stepsTaken;
+      _start_time = -1;
+    }
   }
 
   double time_per_step() {
-    return _total_time / _steps;
+    return total_time() / _steps;
   }
 
   double total_time() {
-    return _total_time;
+    float m_total_time = _total_time;
+    float m_start_time = _start_time;
+
+    if (m_start_time < 0) {
+      return m_total_time;
+    } else {
+      return m_total_time + omp_get_wtime() - m_start_time;
+    }
   }
 };
 
