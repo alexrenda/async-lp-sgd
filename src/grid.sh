1344e202345eb1cf97a42a86fbbbcf0a84f9c99d
Alex Renda <adr74@cornell.edu>
2017-12-11 22:50:39 -0500

    thread grid

--- src/grid.sh
+++ src/grid.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-ALPHAS=( 0.001 0.005 0.01 0.05 0.1 0.5 1.0 5.0 10.0 )
+ALPHAS=( 0.01 0.03 0.05 0.075 0.1 0.3 0.5 0.75 1 )
+THREADS=( 1 2 4 8 12 16 20 24 )
 
 for alpha in ${ALPHAS[@]}; do 
-    qsub job-run.pbs -v GD_TYPE=${1?},GD_ALPHA=$alpha; 
+    for num_threads in ${THREADS[@]}; do
+        qsub job-run.pbs -v GD_TYPE=${1?},GD_ALPHA=$alpha,GD_NUM_THREADS=$num_threads
+    done
 done
